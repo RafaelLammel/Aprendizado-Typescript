ad7c646d381e55e90ed5577bd574f42aca3132eb
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2020-04-20 16:07:40 -0300

    Continuação de tipos em typescript

--- devdojo/scripts/olamundo.js
+++ devdojo/scripts/olamundo.js
@@ -38,4 +38,43 @@ var Day;
 ;
 var day = Day.MONDAY;
 console.log("" + day);
+//Argumentos não obrigatórios
+/*function add(v1:number, v2:number, v3?:number):number {
+  if(v3 !== undefined)
+    return v1+v2+v3;
+  return v1+v2;
+}
+
+console.log(add(1,2));
+console.log(add(1,2,3));*/
+//Usar any para quando não sabemos o tipo do argumento
+function add(v1, v2, v3) {
+    if (v3 !== undefined)
+        return v1 + v2 + v3;
+    return v1 + v2;
+}
+console.log(add('hello', ' world'));
+console.log(add(1, 2, 3));
+var var1 = 'Teste';
+//variáveis do tipo void só podem receber null ou undefined
+var void1 = null;
+var void2 = undefined;
+//Casting
+var stringLenght = var1.length;
+var stringUpperCase = var1.toUpperCase();
+console.log(stringLenght);
+console.log(stringUpperCase);
+//Rest Parameter
+function add2(v1) {
+    var v2 = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        v2[_i - 1] = arguments[_i];
+    }
+    var sum = 0;
+    for (var i = 0; i < v2.length; i++) {
+        sum += v2[i];
+    }
+    console.log(v1 + sum);
+}
+add2("Sum is ", 1, 2, 3, 4);
 //# sourceMappingURL=olamundo.js.map
--- devdojo/scripts/olamundo.ts
+++ devdojo/scripts/olamundo.ts
@@ -45,3 +45,47 @@ enum Day{MONDAY, TUESDAY};
 let day:Day = Day.MONDAY;
 
 console.log(`${day}`);
+
+//Argumentos não obrigatórios
+/*function add(v1:number, v2:number, v3?:number):number {
+  if(v3 !== undefined)
+    return v1+v2+v3;
+  return v1+v2;
+}
+
+console.log(add(1,2));
+console.log(add(1,2,3));*/
+
+//Usar any para quando não sabemos o tipo do argumento
+function add(v1:any, v2:any, v3?:any):any {
+  if(v3 !== undefined)
+    return v1+v2+v3;
+  return v1+v2;
+}
+
+console.log(add('hello',' world'));
+console.log(add(1,2,3));
+
+let var1:any = 'Teste';
+
+//variáveis do tipo void só podem receber null ou undefined
+let void1:void = null;
+let void2:void = undefined;
+
+//Casting
+let stringLenght:number = (<string> var1).length;
+let stringUpperCase:string = (var1 as string).toUpperCase();
+
+console.log(stringLenght);
+console.log(stringUpperCase);
+
+//Rest Parameter
+function add2(v1:string, ...v2:number[]):void {
+  let sum:number = 0;
+  for(let i = 0; i < v2.length; i++) {
+    sum += v2[i];
+  }
+  console.log(v1 + sum);
+}
+
+add2("Sum is ", 1, 2, 3, 4);
